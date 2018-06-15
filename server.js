1815f536c79eca6753b37474f41387b3b983bad7
Rohit Das <rohit.das950@gmail.com>
2018-06-15 11:48:05 +0530

    server.js: Added files for timestamp microservice
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date_string", (req,res) => {
+  var date, date_string = req.params.date_string;
+  if(date_string === "") date = new Date();
+  if(/^\d+$/.test(date_string)) date = new Date(Number(date_string));
+  else if(/\d{4}-\d{2}-\d{2}/.test(date_string)) date = new Date(date_string);
+  else res.json({"unix":null, "utc": "Invalid Date"});
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
